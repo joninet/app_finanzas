51bd62ee7f0bf9c01f84336f2cd9795c3df71717
Jonathan Desplats <jon.desplats@gmail.com>
2026-01-04 20:58:40 -0300

    fix

--- finanzas_personales/settings.py
+++ finanzas_personales/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-3ri1kv(bshkqhd9u!_3v!gb!4hp_!k(!w!k96pyut=q=a7adf9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
